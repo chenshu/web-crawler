26fc0f84c8011e9f80f3c9a35f79c7f25408573e
chen shu <chenshu510@gmail.com>
2012-03-13 13:14:48 +0800

    修改抓取策略，优先抓取引用最多的页面

--- apk_spider.py
+++ apk_spider.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import StringIO
-import gzip
+import sys
 import re
 from spider import Spider
-from collections import defaultdict
 
 class ApkSpider(Spider):
 
-    def parse(self, html):
+    def parse(self, url, html):
+        print url
         urls = re.findall(r'http://static.apk.hiapk.com/html/([0-9/]+)\.html', html)
-        for url in urls:
-            url_id = url.split('/')[-1]
-            new_url = 'http://apk.hiapk.com/SoftDetails.aspx?action=GetBaseInfo&apkId=%s' % (url_id)
-            self.get(new_url, self.parse)
+        return ['http://apk.hiapk.com/SoftDetails.aspx?action=GetBaseInfo&apkId=%s' % (url.split('/')[-1]) for url in urls]
 
 if __name__ == '__main__':
     spider = ApkSpider(name = 'hiapk', host = 'apk.hiapk.com', start_url = '/', timeout = 10)
--- spider.py
+++ spider.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import with_statement
 import os
+import sys
 import os.path
+import cookielib
 from urllib import urlencode
 from urllib2 import urlopen, Request, build_opener, HTTPCookieProcessor, HTTPError
-import cookielib
-import StringIO
 import gzip
+import StringIO
 from collections import defaultdict
 from datetime import datetime
 import time
 
 class Spider(object):
+
+    count = 0
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -28,39 +32,42 @@ class Spider(object):
         # 设置链接超时时间
         if not getattr(self, 'timeout', None):
             self.timeout = 10
-        self.GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
         self.cookie = cookielib.CookieJar()
-        # 已经抓取的页面保存到文件中
-        self.filename = '%s/%s_urls' % (os.path.dirname(os.path.realpath(__file__)), self.name)
-        if not os.path.exists(self.filename):
-            self.fp = open(self.filename, 'a+')
-        else:
-            self.fp = open(self.filename, 'r')
-        # 已经抓取了多少页面
+        self.GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+        # 本次已经抓取或者不需要抓取的页面
+        self.last = defaultdict(dict)
+        # 本次需要抓取的页面
+        self.now = defaultdict(int)
+        # 上次已经抓取的页面
         self.finished = defaultdict(dict)
-        for line in self.fp:
-            data = line.strip().split('\t')
-            url = data[0]
-            last_modified = data[1]
-            expires = data[2]
-            etag = ''
-            if len(data) > 3:
-                etag = data[3]
-            self.finished[url]['last_modified'] = datetime.strptime(last_modified, self.GMT_FORMAT)
-            self.finished[url]['expires'] = datetime.strptime(expires, self.GMT_FORMAT)
-            self.finished[url]['etag'] = etag
-        self.fp.close()
-        self.newed = set()
+        self.filename = '%s/%s_urls' % (os.path.dirname(os.path.realpath(__file__)), self.name)
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as fp:
+                for line in fp:
+                    url, last_modified, expires, etag = line.strip().split('\t')
+                    last_modified = datetime.strptime(last_modified, self.GMT_FORMAT)
+                    expires = datetime.strptime(expires, self.GMT_FORMAT)
+                    self.finished[url]['last_modified'] = last_modified
+                    self.finished[url]['expires'] = expires
+                    self.finished[url]['etag'] = etag
+                    # 已经过期
+                    if expires <= datetime.now():
+                        self.now[url] = 1
+                    # 没有过期
+                    else:
+                        self.last[url] = self.finished[url]
 
+    # 抓取页面，并进行页面分析
     def get(self, url, callback = None):
-        if len(self.newed) % 10 == 0:
-            time.sleep(1)
+        self.count += 1
+        if (self.count % 10 == 0):
+            #print '%s\t%s\t%s' % (len(self.finished), len(self.now), len(self.last))
+            time.sleep(5)
         try:
-            if url in self.newed:
+            # 是否需要抓取
+            if url in self.last:
                 return None
-            if url in self.finished and datetime.now() < self.finished[url]['expires']:
-                return None
-            print url
+
             # 绑定cookie
             self.opener = build_opener(HTTPCookieProcessor(self.cookie))
             # 设置http header
@@ -73,48 +80,72 @@ class Spider(object):
                 ('Host', self.host),
                 ('User-Agent', 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')
             ]
+
             # 获取当前页面的上次修改时间
             last_modified = self.finished[url]['last_modified'] if url in self.finished else None
-            # 获取当前页面的内容hash
-            etag = self.finished[url]['etag'] if url in self.finished else None
             # 设置If-Modified-Since
             if last_modified is not None:
                 self.opener.addheaders.append(('If-Modified-Since', last_modified.strftime(self.GMT_FORMAT)))
+            # 获取当前页面的内容hash
+            etag = self.finished[url]['etag'] if url in self.finished else None
             # 设置If-None-Match
             if etag is not None:
                 self.opener.addheaders.append(('If-None-Match', etag))
+
             # 抓取页面
             response = self.opener.open(url, timeout = self.timeout)
             info = response.info()
             # 获取页面的上次修改时间
-            self.finished[url]['last_modified'] = datetime.strptime(info['Last-Modified'], self.GMT_FORMAT) if 'Last-Modified' in info else datetime.now()
+            last_modified = datetime.strptime(info['Last-Modified'], self.GMT_FORMAT) if 'Last-Modified' in info else datetime.now()
             # 获取页面的过期时间
-            self.finished[url]['expires'] = datetime.strptime(info['Expires'], self.GMT_FORMAT) if 'Expires' in info else datetime.now()
+            expires = datetime.strptime(info['Expires'], self.GMT_FORMAT) if 'Expires' in info else datetime.now()
             # 获取页面的内容hash
-            self.finished[url]['etag'] = info['ETag'] if 'ETag' in info else ''
-            self.newed.add(url)
+            etag = info['ETag'] if 'ETag' in info else None
+
             # gzip解压缩
             data = StringIO.StringIO(response.read())
             gzipper = gzip.GzipFile(fileobj = data)
             if callback is not None:
-                callback(gzipper.read())
+                # 分析页面获取url
+                urls = callback(url, gzipper.read())
+                for new_url in urls:
+                    # 新的url
+                    if not new_url in self.last:
+                        self.now[new_url] = self.now[new_url] + 1 if new_url in self.now else 1
+            # 更新本次已经抓取
+            self.last[url]['last_modified'] = last_modified
+            self.last[url]['expires'] = expires
+            self.last[url]['etag'] = etag
+            # 删除本次需要抓取
+            del self.now[url]
+            #print '200\t%s' % (url)
         except HTTPError, e:
             # 如果返回304
-            if e.code != 304:
+            if e.code == 304:
+                # 更新本次已经抓取
+                self.last[url] = self.finished[url]
+                # 删除本次需要抓取
+                del self.now[url]
+                #print '304\t%s' % (url)
+            else:
                 print e
+        # 筛选引用数最多的页面
+        self.get(sorted(self.now.items(), lambda x, y : cmp(x[1], y[1]), reverse = True)[1][0], self.parse)
         return None
 
     def start(self):
         # 抓取页面
         url = 'http://%s%s' % (self.host, self.start_url)
+        self.now[url] = 1
         self.get(url, self.parse)
+
         # 更新抓取列表
         self.f = open('%s_new' % (self.filename), 'w+')
-        self.f.writelines(['%s\t%s\t%s\t%s%s' % (url, self.finished[url]['last_modified'].strftime(self.GMT_FORMAT), self.finished[url]['expires'].strftime(self.GMT_FORMAT), self.finished[url]['etag'], os.linesep) for url in self.finished])
+        self.f.writelines(['%s\t%s\t%s\t%s%s' % (url, self.last[url]['last_modified'].strftime(self.GMT_FORMAT), self.last[url]['expires'].strftime(self.GMT_FORMAT), self.last[url]['etag'], os.linesep) for url in self.last])
         self.f.close()
         # 替换旧列表
         os.rename('%s_new' % self.filename, self.filename)
 
-    def parse(self, html):
+    def parse(self, url, html):
         raise NotImplementedError
 
